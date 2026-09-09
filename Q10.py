0d99de6f33b380522bfb887342758523c47ff1f8
Jéssica <yukidoom2@gmail.com>
2026-09-08 22:51:20 -0300

    Delete Q10.py

--- Q10.py
+++
@@ -1,9 +0,0 @@
-valor = float(input("Digite o valor financiado: "))
-taxa = float(input("Digite a taxa de juros mensal (%): "))
-meses = int(input("Digite a quantidade de meses: "))
-
-juros = valor * (taxa / 100) * meses
-montante = valor + juros
-
-print("Juros acumulados: R$", juros)
-print("Montante total a pagar: R$", montante)
