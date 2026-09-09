7314f7e3f416b9e75ed196271962740ab4d4abdf
Jéssica <yukidoom2@gmail.com>
2026-09-08 22:51:58 -0300

    Delete Q14.py

--- Q14.py
+++
@@ -1,7 +0,0 @@
-preco = float(input("Digite o preço unitário: "))
-quantidade = int(input("Digite a quantidade: "))
-desconto = float(input("Digite o desconto em reais: "))
-
-total = (preco * quantidade) - desconto
-
-print("Valor final da compra: R$", total)
