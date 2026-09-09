ea13e7e9c6269b9a314a72064691383d8dcdea21
Jéssica <yukidoom2@gmail.com>
2026-09-08 22:49:10 -0300

    Delete Q1.py

--- Q1.py
+++
@@ -1,5 +0,0 @@
-#Entradas
-Horas = int(input("Digite a quantidade de Horas: " ))
-Minutos = int(input("Digite a quantidade de Minutos: "))
-Segundos = (Horas* 3600) + (Minutos*60)
-print(f"Tempo total em segundos: {Segundos}s")
