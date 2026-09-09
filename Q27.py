03babe65e2f3fad748aa9bf21fad56d7d814f49a
Jéssica <yukidoom2@gmail.com>
2026-09-08 22:54:39 -0300

    Delete Q27.py

--- Q27.py
+++
@@ -1,7 +0,0 @@
-usuario = input("Digite o usuário: ")
-senha = input("Digite a senha: ")
-
-if usuario == "Fesor" and senha == "DOm1No":
-    print("Login realizado com sucesso.")
-else:
-    print("Usuário ou senha inválidos.")
